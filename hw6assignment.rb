5e42266010037802c33401741fb6b363efadbe30
IgorMBorushek <igor.borushek@gmail.com>
2013-11-24 11:10:51 -0200

    Initial commit

---
+++ hw6assignment.rb
@@ -0,0 +1,19 @@
+# University of Washington, Programming Languages, Homework 6, hw6runner.rb
+
+# This is the only file you turn in, so do not modify the other files as
+# part of your solution.
+
+class MyPiece < Piece
+	All_My_Pieces
+
+end
+
+class MyBoard < Board
+
+end
+
+class MyTetris < Tetris
+
+end
+
+
